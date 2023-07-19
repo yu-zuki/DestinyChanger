8dbbebc48c13e16273396737f878c6f11f02bef0
YUZUKI <ai1072308083@gmail.com>
2023-07-20 05:10:21 +0900

    CodeAdd

--- Source/DestinyChanger/EnemyAI/MoveToPlayerBT.cpp
+++ Source/DestinyChanger/EnemyAI/MoveToPlayerBT.cpp
@@ -25,16 +25,17 @@ EBTNodeResult::Type UMoveToPlayerBT::ExecuteTask(UBehaviorTreeComponent& OwnerCo
 	UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(GetWorld());
 	if (NavSys == nullptr) return EBTNodeResult::Failed;
 
-	// Playerを取得
+	// Playerの所に移動開始
 	OwnerComp.GetAIOwner()->MoveToActor( UGameplayStatics::GetPlayerCharacter(GetWorld(), 0), 50 );
 	return EBTNodeResult::InProgress;
 }
 
 void UMoveToPlayerBT::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
-	// AIControllerの状態がIdleなら
+	// AIControllerの状態がIdleなら（プレイヤーの所にたどり着いたら）
 	if (OwnerComp.GetAIOwner()->GetMoveStatus() == EPathFollowingStatus::Idle) {
 
+		//Stateをプレイヤーを攻撃する状態に変更
 		OwnerComp.GetBlackboardComponent()->SetValueAsEnum("EnemyState", (uint8)EEnemyState::Attack);
 		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
 	}
